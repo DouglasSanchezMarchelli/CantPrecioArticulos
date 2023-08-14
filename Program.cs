d8d9bb425baf99e0c1cdcc6ecf34d14f2e13f06b
Douglas Sánchez <142137936+DouglasSanchezMarchelli@users.noreply.github.com>
2023-08-14 14:55:39 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrecioProducto
+{
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Precios por artículo de Compra");
+
+Console.Write("Ingrese el precio del artículo: ");
+double precio = double.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad que lleva el cliente: ");
+int cantidad = int.Parse(Console.ReadLine());
+
+double totalPagar = precio * cantidad;
+
+Console.WriteLine("Total a pagar:$ "+ totalPagar);
+     }
+  }
+}
